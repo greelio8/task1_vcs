0855c230c0af3a81ac94916a0c082a2b014624e5
dimatrubca <dimatrubca@gmail.com>
2021-02-16 16:37:36 +0200

    Add circle

---
+++ Geometry/Geometry/Circle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Geometry
+{
+     class Circle
+     {
+          public int Radius { get; set; }
+
+          public Circle(int radius) {
+               this.Radius = radius;
+          }
+     }
+}

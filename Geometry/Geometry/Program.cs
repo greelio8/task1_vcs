a53b360aec92e56710ebee94267d66dfa5ae95f0
dimatrubca <dimatrubca@gmail.com>
2021-02-16 16:27:59 +0200

    Modify rect width to 1

--- Geometry/Geometry/Program.cs
+++ Geometry/Geometry/Program.cs
@@ -5,7 +5,7 @@ namespace Geometry
      class Program
      {
           static void Main(string[] args) {
-               Rectangle rect = new Rectangle(5, 1);
+               Rectangle rect = new Rectangle(1, 1);
 
                Console.WriteLine(rect.Area());
           }
